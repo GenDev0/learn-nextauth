ca352a595e94e28b597209ba3b81175464e869ba
GenDev0 <117724882+GenDev0@users.noreply.github.com>
2023-01-05 11:04:08 +0100

    Session

--- components/WelcomePage.tsx
+++ components/WelcomePage.tsx
@@ -3,12 +3,8 @@ import { useSession, signIn, signOut } from "next-auth/react";
 import { unstable_getServerSession } from "next-auth/next";
 import { Session } from "next-auth";
 
-type Props = {
-  session: Session | null;
-};
-
-export default function WelcomePage({ session }: Props) {
-  // const { data: session } = useSession();
+export default function WelcomePage() {
+  const { data: session } = useSession();
   console.log(session);
 
   if (session) {
